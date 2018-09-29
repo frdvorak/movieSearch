8c5e110564918d61f9f7f9d08f49f3ee7400ce08
unknown <frantisek.dvorak011@gmail.com>
2018-09-29 12:49:02 +0200

    pulling movies from array in state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('This is my initializer!');
+    
+
+    const movies = [
+      {id: 0, title: 'Avengers', overview: 'asdas ahsd kfjojlwf kasdja'},
+      {id: 1, title: 'Grease', overview: 'asdas ahsd kfjojlwf kasdja'},
+      {id: 2, title: 'Lalaland', overview: 'asdas ahsd kfjojlwf kasdja'},
+      {id: 3, title: 'Terminator', overview: 'asdas ahsd kfjojlwf kasdja'},
+    ]
+
+    var movieRows = [];
+    movies.forEach((movie)=> {
+      const movieRow = <table key={movie.id}>
+        <tbody>
+          <tr>
+            <td>
+              <img src="" width='80' alt="poster"/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              {movie.title}
+              <p>{movie.overview}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      movieRows.push(movieRow)
+    })
+
+    this.state = {rows: movieRows}
+  }
+
+  
+
   render() {
     return (
       <div className="App">
@@ -17,9 +54,11 @@ class App extends Component {
             </tr>
           </tbody>
         </table>
-
-      <input className="main-input" placeholder="Enter search term"></input>
-
+      <input className="main-input" placeholder="Enter search term"/>
+      
+      <div className='app-body'>
+        {this.state.rows}
+      </div>
       </div>
     );
   }
